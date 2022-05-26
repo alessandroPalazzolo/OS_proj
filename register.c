b866a4b6d8d9c6eceda2cd4fc88c2c1e014a5c7f
Alessandro Palazzolo <alessandrorochepalazzolo@gmail.com>
2022-05-26 18:41:13 +0200

    first stage socket-utils

--- register.c
+++ register.c
@@ -1,32 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h> 
+#include "socket-utils.h"
 
 int main(int argc, char* argv[]) {
-  signal(SIGCHLD, SIG_IGN);
-  int serverFd, clientFd, serverLen, clientLen;
-  struct sockaddr_un serverUNIXAddress;
-  struct sockaddr_un clientUNIXAddress;
-  struct sockaddr* serverSockAddrPtr;
-  struct sockaddr* clientSockAddrPtr;
-  
-  serverSockAddrPtr = (struct sockaddr*) &serverUNIXAddress;
-  clientSockAddrPtr = (struct sockaddr*) &clientUNIXAddress;
-  serverLen = sizeof(serverUNIXAddress);
-  clientLen = sizeof(clientUNIXAddress);
-  serverFd = socket(AF_UNIX, SOCK_STREAM, 0);
-  serverUNIXAddress.sun_family = AF_UNIX;
-  strcpy(serverUNIXAddress.sun_path, "register_socket");
-  unlink("register_socket");
-  bind(serverFd, serverSockAddrPtr, serverLen);
-  listen(serverFd, 5); // numero massimo di connessioni pendenti
-  while(1){
-    clientFd = accept (serverFd, clientSockAddrPtr, &clientLen);
-    
-  }
+  SocketDetails sock;
+  sock = initSocket("register_socket");
+  buildSocket(sock, 5);
+  runSocket(sock);
 }
 
--- train.c
+++ train.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/types.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h> 
 #include <errno.h>
 #include <stdbool.h>
 
@@ -12,18 +13,35 @@
 void getMap();
 
 int main(int argc, char* argv[]){
+  int clientFd, serverLen, result;
+  struct sockaddr_un serverUNIXAddress;
+  struct sockaddr* serverSockAddrPtr;
+  serverSockAddrPtr = (struct sockaddr*) &serverUNIXAddress;
+  serverLen = sizeof (serverUNIXAddress);
+  clientFd = socket (AF_UNIX, SOCK_STREAM, 0);
+  serverUNIXAddress.sun_family = AF_UNIX; /* Server domain */
+  strcpy (serverUNIXAddress.sun_path, "register_socket");/*Server name*/
+  do { 
+  result = connect (clientFd, serverSockAddrPtr, serverLen);
+  if (result == -1) sleep (1); /* Wait and then try again */
+  } while (result == -1);
+  char socketOutput[10];
+  read(clientFd, socketOutput, 8);
+  printf("%s\n", socketOutput);
+  close (clientFd); /* Close the socket */
+  exit (/* EXIT_SUCCESS */ 0); /* Done */
   
-  Train train;
-  strcpy(train.name, argv[1]);
-  
-  bool arrived = false;
-  
-  getMap(train.name, train.route);
+  // Train train;
+  // strcpy(train.name, argv[1]);
 
-  while(!arrived){
+  // bool arrived = false;
+  
+  // getMap(train.name, train.route);
+
+  // while(!arrived){
     
 
-  } 
+  // } 
 }
 
 
